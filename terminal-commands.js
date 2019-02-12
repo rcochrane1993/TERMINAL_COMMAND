e2b678edc7c3f358eb6a96ba41f07f368080f1fd
Robert Cochrane <rcochrane1105@gmail.com>
2019-02-12 18:30:07 -0500

    working with core node libraries

---
+++ terminal-commands.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+
+module.exports.ls = () => {
+  fs.readdir('./', (err, files) => {
+    const filesToString = files.reduce((acc, file) => {
+      return `${acc} ${file} `;
+    }, '');
+
+    console.log(filesToString);
+  });
+};
+
+module.exports.touch = (input) => {
+    fs.writeFile('input', '', 'utf8', (err) => {
+        if (err) throw err;
+      
+        console.log('file created')
+      });
+
+/*module.exports.mkdir = () => {
+  // your code here
+};*/
